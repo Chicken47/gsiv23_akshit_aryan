aa7b858df685b6bb2ce559a629b0e1a8cf8c9c79
Akshit Aryan <aryanakshit152@gmail.com>
2023-08-24 09:57:11 +0530

    readme + loaders

--- README.md
+++ README.md
@@ -12,3 +12,20 @@ You may also see any lint errors in the console.
 
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### Key Achievements Demonstrating Proficiency
+
+- Dynamic Routing using react-router-dom: instead of calling another API for searching the specific title on details page, use global state of movie list to filter out thte result.
+- Global State Management: I jhave worked with redux, contextAPI and Recoil. Recoil has been the easiest to use and the most react-y with the lease boilerplate code and there are hooks as well for ease-of-use.
+- Fully Responsive Implementation: used tailwind css which made it easier to write responsive components. (can be converted to normal css as well using extensions/tools if required, i can also write normal css, if using tailwindcss is an issue).
+- API Integration: i usually use axios for my API calls.
+
+### List what you would do to improve your solution if you had 4 more hours available for this task
+
+- Error Handling: better ux
+- Unit Testing: I dont really have experience in test driven development. I have written tests in cypress in the past but that was to train a new joinee.
+- Loaders (might end up adding, can get annoying without)
+
+### Any feedback about how we may improve this challenge
+
+- freedom to change design to make it a better ux. (personal opinion). otherwise this challenge is comprehensive and gets most of the concepts of coding in react. (i was a little confused with the usage of the words 'recommend', I did start it with TDD but the progress was painstakingly slow. When i restarted without it took me about 4 hours to make.)
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -7,26 +7,10 @@ import { Link } from "react-router-dom";
 const MovieList = () => {
   const [movies, setMovies] = useRecoilState(movieList);
   const [searchQuery, setSearchquery] = useState("");
-  useEffect(() => {
-    setSearchquery("");
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTEzNzJlZjE5NDQ5YjI4MzE1NWIwYjE5M2Q2Yjk3MiIsInN1YiI6IjY0ZDEyMzA5ZDhkMzI5MDExZTc0YjQwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mx3HUCJnZ96zwrtpC5F5XU_9BIjika6PyFpiHgROrBA",
-      },
-    };
-
-    fetch("https://api.themoviedb.org/3/movie/upcoming", options)
-      .then((response) => response.json())
-      .then((response) => {
-        setMovies(response.results);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  const [loading, setLoading] = useState(false);
 
   const upcomingMovies = () => {
+    setLoading(true);
     const options = {
       method: "GET",
       headers: {
@@ -39,10 +23,19 @@ const MovieList = () => {
     fetch("https://api.themoviedb.org/3/movie/upcoming", options)
       .then((response) => response.json())
       .then((response) => {
+        setLoading(false);
         setMovies(response.results);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        alert(err);
+        setLoading(false);
+      });
   };
+
+  useEffect(() => {
+    setSearchquery("");
+    upcomingMovies();
+  }, []);
 
   useEffect(() => {
     if (searchQuery === "") {
@@ -75,12 +68,12 @@ const MovieList = () => {
   };
 
   return (
-    <div className="w-full flex flex-col bg-white font-sans">
-      <div className="w-full px-5 py-3 shadow-lg flex justify-between items-center">
-        <div className="flex md:w-1/2 w-10/12 space-x-5 items-center">
+    <div className="flex flex-col w-full font-sans bg-white">
+      <div className="flex items-center justify-between w-full px-5 py-3 shadow-lg">
+        <div className="flex items-center w-10/12 space-x-5 md:w-1/2">
           <input
             placeholder="Search Movies"
-            className="px-3 w-full py-2 bg-gray-200 rounded text-black transition-all"
+            className="w-full px-3 py-2 text-black transition-all bg-gray-200 rounded"
             value={searchQuery}
             onChange={(e) => setSearchquery(e.target.value)}
           />
@@ -107,18 +100,27 @@ const MovieList = () => {
           />
         </span>
       </div>
-      <div className="w-full flex flex-wrap justify-around">
-        {movies?.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            rating={movie.vote_average}
-            posterLink={movie.poster_path}
-            description={movie.overview}
-            movieId={movie.id}
-          />
-        ))}
-      </div>
+      {!loading ? (
+        <div className="flex flex-wrap justify-around w-full">
+          {movies?.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              title={movie.title}
+              rating={movie.vote_average}
+              posterLink={movie.poster_path}
+              description={movie.overview}
+              movieId={movie.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex flex-col items-center mt-[5vw]">
+          <span className="h-1/2">
+            <img src="https://i.gifer.com/XOsX.gif" />
+          </span>
+          <span className="font-bold text-[5vw]">Loading...</span>
+        </div>
+      )}
     </div>
   );
 };
